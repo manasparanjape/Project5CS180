527e3169450cf83049b6735d7af3f54e140f7017
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-13 19:35:15 -0500

    Update AccountsTesting.java

--- src/AccountsTesting.java
+++ src/AccountsTesting.java
@@ -1,8 +1,10 @@
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class AccountsTesting {
   //Tester class used to test each method included in the Accounts class
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     
     //Creating Accounts object representing a student account
     Accounts studentOne = new Accounts("Billy23", "B1L@#", "Billy", "Smith", false);
@@ -20,11 +22,12 @@ public class AccountsTesting {
     } catch(FileNotFoundException e) {
     	System.out.println("File Not Found");
     }
-    
-    
-    
-    
-    
+
+
+    //main method in Accounts that tests full functionality of the project
+    //Tests capabilities of the program to create, login, and logout of an account
+    //Tests whether students can open courses and view forums and points. 
+    Accounts.main(args);
     
     
   }
