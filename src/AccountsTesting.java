1f22a980efd443878c8c5ee69c07186bb46b50e5
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-14 18:09:26 -0500

    Update AccountsTesting.java

--- src/AccountsTesting.java
+++ src/AccountsTesting.java
@@ -6,14 +6,24 @@ public class AccountsTesting {
   //Tester class used to test each method included in the Accounts class
   public static void main(String[] args) throws Exception {
     
+    Accounts studentOne = null;
     //Creating Accounts object representing a student account
     Accounts studentOne = new Accounts("Billy23", "B1L@#", "Billy", "Smith", false);
-    Accounts teacherOne = new Accounts("Ms.Paul", "skooliskool", "Mary", "Paul", true);
-    //successfully created
+    if(studentOne != null) {
+      System.out.println(true);
+    } else {
+      System.out.println(false);
+    }
+    //successfully created if true
     
     //Testing covertToString method in Accounts.java
     String studentString = studentOne.convertToString();
     System.out.println(studentString.equals("Billy23---B1L@#---Billy---Smith---student"));
+    //returns true if method matches expected output
+   
+    //Creating Accounts object representing a teacher account
+    Accounts teacherOne = new Accounts("Ms.Paul", "skooliskool", "Mary", "Paul", true);
+    //Testing covertToString method in Accounts.java
     String teacherString = teacherOne.convertToString();
     System.out.println(studentString.equals("Ms.Paul---skooliskool---Mary---Paul---teacher"));
     //returns true if method matches expected output
@@ -28,10 +38,11 @@ public class AccountsTesting {
     
     //checks if username is already in use; true if username is used and false otherwise
     System.out.println(studentOne.checkUsernameAvailability("Billy23"));
-    //test correctly returns true as the user Billy23 was previously created
     
     //main method in Accounts that tests full functionality of the project
-    //Tests capabilities of the program to create, login, and logout of an account
+    //Tests capabilities of the program to create, delete, login, and logout of an account
+    //Tests if Teachers can create, edit, and delete forums
+    //Tests if Students can create replies
     //Tests whether students can open courses and view forums and points. 
     //Tests methods that require all five classes to work together
     Accounts.main(args);
