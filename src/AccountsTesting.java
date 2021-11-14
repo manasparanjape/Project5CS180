06e8b36f64d0d6341c768b96c7b5931f2404720e
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-14 17:50:56 -0500

    Update AccountsTesting.java

--- src/AccountsTesting.java
+++ src/AccountsTesting.java
@@ -8,13 +8,15 @@ public class AccountsTesting {
     
     //Creating Accounts object representing a student account
     Accounts studentOne = new Accounts("Billy23", "B1L@#", "Billy", "Smith", false);
+    Accounts teacherOne = new Accounts("Ms.Paul", "skooliskool", "Mary", "Paul", true);
+    //successfully created
     
     //Testing covertToString method in Accounts.java
     String studentString = studentOne.convertToString();
-    System.out.println(studentString);
-    //output:
-    //Billy23---B1L@#---Billy---Smith---student
-    //matches expected output
+    System.out.println(studentString.equals("Billy23---B1L@#---Billy---Smith---student"));
+    String teacherString = teacherOne.convertToString();
+    System.out.println(studentString.equals("Ms.Paul---skooliskool---Mary---Paul---teacher"));
+    //returns true if method matches expected output
     
     //check if writeToFile method returns a FileNotFoundException
     try {
@@ -22,13 +24,11 @@ public class AccountsTesting {
     } catch(FileNotFoundException e) {
     	System.out.println("File Not Found");
     }
+    //passed; does not return file not found
     
-    //readFile() method
-    studentOne.readFile();
-    
-    //checks if username is available
+    //checks if username is already in use; true if username is used and false otherwise
     System.out.println(studentOne.checkUsernameAvailability("Billy23"));
-
+    //test correctly returns true as the user Billy23 was previously created
     
     //main method in Accounts that tests full functionality of the project
     //Tests capabilities of the program to create, login, and logout of an account
