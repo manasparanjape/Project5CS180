bf76b21a1c7c8aee28116496394dc950000a55c7
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-13 19:05:26 -0500

    Update AccountsTesting.java

--- src/AccountsTesting.java
+++ src/AccountsTesting.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+
 public class AccountsTesting {
   //Tester class used to test each method included in the Accounts class
   public static void main(String[] args) {
@@ -10,6 +12,20 @@ public class AccountsTesting {
     System.out.println(studentString);
     //output:
     //Billy23---B1L@#---Billy---Smith---student
+    //matches expected output
+    
+    //check if writeToFile method returns a FileNotFoundException
+    try {
+    	studentOne.writeToFile();
+    } catch(FileNotFoundException e) {
+    	System.out.println("File Not Found");
+    }
+    
+    
+    
+    
+    
+    
+    
   }
 }
- 
