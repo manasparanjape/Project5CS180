2aa3b1c4bff7208af4a0f6e295da0bf6e3f75c6e
123kevinma123 <89763021+123kevinma123@users.noreply.github.com>
2021-11-13 22:46:10 -0500

    Update Accounts.java
    added error handling

--- src/Accounts.java
+++ src/Accounts.java
@@ -209,30 +209,32 @@ public class Accounts {
             try {
                 System.out.println(initialPrompt);
                 option = scan.nextInt();
-                scan.nextLine();
-                if (option < 1 || option > 4) {
-                    System.out.println("You entered an invalid option. Please enter a number between 1 and 4.");
-                } else {
-                    switch(option) {
-                        case 1 -> {
-                            int accountCheck = securityCheck(scan);
-                            Main main;
-                            if (accountCheck == 1) {
-                                findAccount(username);
-                                main = new Main(username, firstName, lastName, true);
-                                main.accountMainMethod();
-                            } else if (accountCheck == 2) {
-                                findAccount(username);
-                                main = new Main(username, firstName, lastName, false);
-                                main.accountMainMethod();
-                            }
-                        }
-                        case 2 -> getNewAccountDetails(scan);
-                        case 3 -> deleteAccount(scan);
-                    }
-                }
-            } catch (InputMismatchException e) {
+            } catch (Exception e) {
                 System.out.println("You did not input an integer. Please input an integer between 1 and 4.");
+                option = 0;
+                scan.nextLine();
+                continue;
+            }
+            if (option < 1 || option > 4) {
+                System.out.println("You entered an invalid option. Please enter a number between 1 and 4.");
+            } else {
+                switch(option) {
+                	case 1 -> {
+                		int accountCheck = securityCheck(scan);
+                		Main main;
+                        if (accountCheck == 1) {
+                        	findAccount(username);
+                            main = new Main(username, firstName, lastName, true);
+                            main.accountMainMethod();
+                        } else if (accountCheck == 2) {
+                        	findAccount(username);
+                        	main = new Main(username, firstName, lastName, false);
+                        	main.accountMainMethod();
+                        }
+                    }
+                    case 2 -> getNewAccountDetails(scan);
+                    case 3 -> deleteAccount(scan);
+                }
             }
         }
         scan.close();
