6ea5b2842dded315c6770cd70e8697960e90b133
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-14 19:13:47 -0500

    Create AccountsTester.java

---
+++ src/AccountsTester.java
@@ -0,0 +1,22 @@
+public class AccountsTester {
+  
+  public static void main(String[] args) {
+    
+    //main method in Accounts that tests full functionality of the project
+    //Accounts test:
+    //Tests capabilities of the program to create, delete, login, and logout of an account
+    //[] indicate input
+    /*
+    
+    
+    */
+    
+    
+    
+    //Tests if Teachers can create, edit, and delete forums
+    //tests if Teachers can reply to student responses.
+    Accounts.main(args);
+    
+  }
+  
+}
