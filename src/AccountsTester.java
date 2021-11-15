a7f22d9cdd26fcd3f5b13a3ffe92910c8ca9ae8d
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-14 19:47:11 -0500

    Update AccountsTester.java

--- src/AccountsTester.java
+++ src/AccountsTester.java
@@ -11,6 +11,7 @@ public class AccountsTester {
     //Accounts test:
     //Tests capabilities of the program to create, delete, login, and logout of an account
     //[] indicate input
+    //Test: Creating account and loggging in and logging out
     /*
     Welcome!
     What would you like to do today?
@@ -33,6 +34,31 @@ public class AccountsTester {
     2. Student
     [1]
     Your account was successfully created
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [1]
+    Please enter your username.
+    [Ms.Hibblestein]
+    Please enter your password
+    [hibblestein2293]
+    Please enter the option number of what you want to do.
+    1) Create course
+    2) Open course
+    3) Log out
+    [3]
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [4]
+    */
+    //Test: Logging into an existing account
+    /*
+    Welcome!
     What would you like to do today?
     1) Login to my account
     2) Create new account
