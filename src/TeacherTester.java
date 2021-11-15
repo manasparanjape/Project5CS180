3db963a258e493e4e94100b88ab7abf3e76b8117
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-14 19:03:35 -0500

    Update TeacherTester.java

--- src/TeacherTester.java
+++ src/TeacherTester.java
@@ -7,7 +7,7 @@ public class TeacherTester {
   public static void main(String[] args) throws Exception {
     //Creating Accounts object representing a teacher account
     Accounts teacherOne = null;
-    Accounts teacherOne = new Accounts("Ms.Paul", "skooliskool", "Mary", "Paul", true);
+    teacherOne = new Accounts("Ms.Paul", "skooliskool", "Mary", "Paul", true);
     if(teacherOne != null) {
       System.out.println(true);
     } else {
@@ -17,7 +17,7 @@ public class TeacherTester {
     
     //Testing covertToString method in Accounts.java
     String teacherString = teacherOne.convertToString();
-    System.out.println(studentString.equals("Ms.Paul---skooliskool---Mary---Paul---teacher"));
+    System.out.println(teacherString.equals("Ms.Paul---skooliskool---Mary---Paul---teacher"));
     //returns true if method matches expected output
     
     //check if writeToFile method returns a FileNotFoundException
@@ -29,7 +29,7 @@ public class TeacherTester {
     //passed; does not return file not found
     
     //checks if username is already in use; true if username is used and false otherwise
-    System.out.println(studentOne.checkUsernameAvailability("Ms.Paul"));
+    System.out.println(teacherOne.checkUsernameAvailability("Ms.Paul"));
     
     //main method in Accounts that tests full functionality of the project
     //Tests capabilities of the program to create, delete, login, and logout of an account
