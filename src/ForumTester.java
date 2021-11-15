c5cb7ead6f3b90fb403bd60e4093b986b076d212
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-14 20:18:54 -0500

    Create ForumTester.java

---
+++ src/ForumTester.java
@@ -0,0 +1,13 @@
+public class ForumTester {
+  
+  public static void main(String[] args) throws Exception {
+    
+    //main method in Accounts that tests full functionality of the project
+    //Forum test:
+    //Tests capabilities of the program create, edit, delete, reply, upvote, and grade discussion forums and posts.
+    //[] indicate input
+    
+    Accounts.main(args);
+    
+  }
+}
