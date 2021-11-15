8154ade4238d66afa4d1abf0aaa30bb4387897d4
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-11-14 20:09:19 -0500

    Update and rename AccountsTester.java to LoginTester.java

---
+++ src/LoginTester.java
@@ -0,0 +1,194 @@
+
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class LoginTester {
+  
+  public static void main(String[] args) throws Exception {
+    
+    //main method in Accounts that tests full functionality of the project
+    //Accounts test:
+    //Tests capabilities of the program to create, delete, login, and logout of an account
+    //[] indicate input
+    
+    Accounts.main(args);
+    
+    //Test: Creating teacher account and loggging in and logging out
+    /*
+    Welcome!
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [2]
+    Set a new username:
+    All usernames are case insensitive.
+    [Ms.Hibblestein]
+    Set a new password:
+    [hibblestein2293]
+    Enter your first name:
+    [Stacy]
+    Enter your last name:
+    [Hibblestein]
+    Are you a teacher or a student?
+    1. Teacher
+    2. Student
+    [1]
+    Your account was successfully created
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [1]
+    Please enter your username.
+    [Ms.Hibblestein]
+    Please enter your password
+    [hibblestein2293]
+    Please enter the option number of what you want to do.
+    1) Create course
+    2) Open course
+    3) Log out
+    [3]
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [4]
+    */
+    //Test: Logging into an existing teacher account
+    /*
+    Welcome!
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [1]
+    Please enter your username.
+    [Ms.Hibblestein]
+    Please enter your password
+    [hibblestein2293]
+    Please enter the option number of what you want to do.
+    1) Create course
+    2) Open course
+    3) Log out
+    [3]
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [4]
+    */
+
+    //Test: Creating student account and loggging in and logging out 
+    /*
+    Welcome!
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [2]
+    Set a new username:
+    All usernames are case insensitive.
+    [SachinP]
+    Set a new password:
+    [sp!2]
+    Enter your first name:
+    [Sachin]
+    Enter your last name:
+    [Patel]
+    Are you a teacher or a student?
+    1. Teacher
+    2. Student
+    [2]
+    Your account was successfully created
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [1]
+    Please enter your username.
+    [SachinP]
+    Please enter your password
+    [sp!2]
+    Please enter the option number of what you want to do.
+    1) Create course
+    2) Open course
+    3) Log out
+    [3]
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [4]
+    */
+    //Test: Logging into an existing student account
+    /*
+    Welcome!
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [1]
+    Please enter your username.
+    [SachinP]
+    Please enter your password
+    [sp!2]
+    Please enter the option number of what you want to do.
+    1) Create course
+    2) Open course
+    3) Log out
+    [3]
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [4]
+    */
+    
+    //Error Tests
+    //Test: Input string instead of integer and input integer out of bounds
+    /*
+    Welcome!
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [login]
+    You did not input an integer. Please input an integer between 1 and 4.
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [sign in]
+    You did not input an integer. Please input an integer between 1 and 4.
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [6]
+    You entered an invalid option. Please enter a number between 1 and 4.
+    What would you like to do today?
+    1) Login to my account
+    2) Create new account
+    3) Delete my account
+    4) Exit
+    [4]
+    */
+    
+  }
+  
+}
